8b4c05ec050eb1fcf0159165eaad0cfcd790f01e
feng_bigred <lifen@iupui.edu>
2017-03-19 09:20:47 -0400

    if block size = submatrix size, works

--- CUDA_TEST/dot.cu
+++ CUDA_TEST/dot.cu
@@ -4,11 +4,11 @@ __global__ void dot(int *a, int *b, int *c){
     __shared__ int tmp[THREADS_PER_BLOCK];
     int index = threadIdx.x + blockIdx.x*blockDim.x;
     tmp[threadIdx.x] = a[index] *b[index];
-    __syncthread();
+    __syncthreads();
 
     if(0 == threadIdx.x){
         int sum = 0;
-        for(int i =0; i< THREADS_PER_THREADS; i++){
+        for(int i =0; i< THREADS_PER_BLOCK; i++){
             sum += tmp[i];
         }
         atomicAdd(c , sum);
